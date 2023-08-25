66fe9e6f8f65ef8b84c87b9e6fa2d1c872f0cb4e
JEngHAng <jangg500z@gmail.com>
2023-08-25 12:31:16 +0700

    operator

---
+++ test005.py
@@ -0,0 +1,12 @@
+# one by one
+# small to Big
+print ("Bangna" > "Bangbon")    #True
+print ("a" > "A")               #True
+print ("SAU" > "sau")           #False
+
+#logical Operator
+#and or not
+
+# ---------------------------------->
+# * / % // , + - , < > = == != <= >= , not , and , ro
+print (7+3*2-5)
